0a23f74461073e18e1f1e4977c6e5a75c83d0b54
manasye <manasyebukit@gmail.com>
2018-07-08 21:35:26 +0700

    Add some improvement

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './OrderSummary.css';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = ( props ) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return <p key = { igKey }> <span style = {{textTransform: 'capitalize'}}> 
-                    { igKey } </span> : { props.ingredients[igKey] } </p>;
-        });
+class OrderSummary extends Component {
+    componentDidUpdate() {
+        
+    }
+
+  render() {
+    const ingredientSummary = Object
+      .keys(this.props.ingredients)
+      .map(igKey => {
+        return <p key={igKey}>
+          <span style={{
+            textTransform: 'capitalize'
+          }}>
+            {igKey}
+          </span>
+          : {this.props.ingredients[igKey]}
+        </p>;
+      });
 
     return (
-        <div className = { classes.OrderSummary }>
-            <h3>You Order</h3>
-            <p>Burger with ingredients: </p>
-            { ingredientSummary }
-            <p>Continue to checkout?</p>
-            <Button btnType = "Danger" clicked = { props.purchaseCancelled }>CANCEL</Button>
-            <Button btnType = "Success" clicked = { props.purchaseContinued }>CONTINUE</Button>
-        </div>
+      <div className={classes.OrderSummary}>
+        <h3>You Order</h3>
+        <p>Burger with ingredients:
+        </p>
+        {ingredientSummary}
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </div>
     );
-};
+  }
+ };
 
-export default orderSummary;
+export default OrderSummary;
