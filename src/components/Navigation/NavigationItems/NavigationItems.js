ce7c0f4ffe84449c8df87b959c7aadd5723f17ba
manasye <manase32@gmail.com>
2018-08-09 22:23:48 +0700

    Trying to use unit test with jest and enzyme

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-import classes from "./NavigationItems.css";
-import NavigationItem from "./NavigationItem/NavigationItem";
+import classes from './NavigationItems.css';
+import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = props => (
     <ul className={classes.NavigationItems}>
@@ -9,12 +9,12 @@ const navigationItems = props => (
             Burger Builder
         </NavigationItem>
         {props.isAuth ? (
-            <NavigationItem link="/orders"> Orders </NavigationItem>
+            <NavigationItem link="/orders">Orders</NavigationItem>
         ) : null}
         {!props.isAuth ? (
-            <NavigationItem link="/auth"> Authenticate </NavigationItem>
+            <NavigationItem link="/auth">Authenticate</NavigationItem>
         ) : (
-            <NavigationItem link="/logout"> Logout </NavigationItem>
+            <NavigationItem link="/logout">Logout</NavigationItem>
         )}
     </ul>
 );
